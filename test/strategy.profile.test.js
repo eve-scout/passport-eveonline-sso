621aaa60c8f07d6869f09daf36416cca06b3b01a
Johnny Splunk <johnnysplunk@eve-scout.com>
2018-05-08 16:36:54 -0700

    Accommodate EVE SSO via Steam, Facebook, etc
    EVE Online SSO can return User and Character tokens. User tokens are returned when no character is associated. Don't assume we will get CharacterID, CharacterName, CharacterOwnerHash values.
    
    - Also includes fixes for example app.
    - Tagged with 0.9.1

--- test/strategy.profile.test.js
+++ test/strategy.profile.test.js
@@ -6,7 +6,7 @@ var EveOnlineSsoStrategy = require('../lib/strategy'),
 
 describe('Strategy#userProfile', function() {
 
-  describe('fetched from default endpoint', function() {
+  describe('fetched from default endpoint with character token', function() {
     var strategy = new EveOnlineSsoStrategy({
         clientID: 'ABC123',
         clientSecret: 'secret',
@@ -17,8 +17,13 @@ describe('Strategy#userProfile', function() {
       if (url != 'https://login.eveonline.com/oauth/verify') { return callback(new Error('incorrect url argument')); }
       if (accessToken != 'token') { return callback(new Error('incorrect token argument')); }
       var body = {
-          'CharacterID'       : 95158478,
-          'CharacterName'     : 'Johnny Splunk',
+          TokenType: 'Character',
+          Scopes: '',
+          ExpiresOn: '2018-05-08T19:50:08',
+          IntellectualProperty: 'EVE',
+          CharacterID: 95158478,
+          CharacterName: 'Johnny Splunk',
+          CharacterOwnerHash: 'Y99STz2ksMHxGrkREO5FAlZVhik='
         };
       callback(null, JSON.stringify(body), undefined);
     };
@@ -37,6 +42,60 @@ describe('Strategy#userProfile', function() {
       expect(profile.provider).to.equal('eveonline-sso');
       expect(profile.CharacterID).to.equal(95158478);
       expect(profile.CharacterName).to.equal('Johnny Splunk');
+      expect(profile.CharacterOwnerHash).to.equal('Y99STz2ksMHxGrkREO5FAlZVhik=');
+      expect(profile.TokenType).to.equal('Character');
+      expect(profile.Scopes).to.equal('');
+      expect(profile.ExpiresOn.toString()).to.equal(new Date('2018-05-08T19:50:08').toString());
+      expect(profile.IntellectualProperty).to.equal('EVE');
+    });
+
+    it('should set raw property', function() {
+      expect(profile._raw).to.be.a('string');
+    });
+
+    it('should set json property', function() {
+      expect(profile._json).to.be.an('object');
+    });
+  }); // fetched from default endpoint
+
+  describe('fetched from default endpoint with user token', function() {
+    var strategy = new EveOnlineSsoStrategy({
+        clientID: 'ABC123',
+        clientSecret: 'secret',
+        callbackURL: 'https://www.example.com/oath/callback'
+      }, function() {});
+
+    strategy._oauth2.get = function(url, accessToken, callback) {
+      if (url != 'https://login.eveonline.com/oauth/verify') { return callback(new Error('incorrect url argument')); }
+      if (accessToken != 'token') { return callback(new Error('incorrect token argument')); }
+      var body = {
+        TokenType: 'User',
+        Scopes: '',
+        ExpiresOn: '2018-05-08T19:50:08',
+        IntellectualProperty: 'EVE'
+      };
+      callback(null, JSON.stringify(body), undefined);
+    };
+
+    var profile;
+
+    before(function(done) {
+      strategy.userProfile('token', function(err, p) {
+        if (err) { return done(err); }
+        profile = p;
+        done();
+      });
+    });
+
+    it('should parse profile', function() {
+      expect(profile.provider).to.equal('eveonline-sso');
+      expect(profile.TokenType).to.equal('User');
+      expect(profile.Scopes).to.equal('');
+      expect(profile.ExpiresOn.toString()).to.equal(new Date('2018-05-08T19:50:08').toString());
+      expect(profile.CharacterID).to.equal(null);
+      expect(profile.CharacterName).to.equal(null);
+      expect(profile.CharacterOwnerHash).to.equal(null);
+      expect(profile.IntellectualProperty).to.equal('EVE');
     });
 
     it('should set raw property', function() {
